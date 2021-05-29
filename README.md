8358fdd262626a84cbb9cc4d3bc643472fc0d2ce
Ali Khajeh-Hosseini <alikhajeh1@googlemail.com>
2021-05-29 18:44:07 +0100

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# azure-devops-github-demo
+# Infracost Azure DevOps with GitHub Demo
+
+See [this pull request](TODO) for the demo.
+
+The [Infracost Azure DevOps integration](https://gitlab.com/infracost/infracost-azure-devops) runs [Infracost](https://infracost.io) against pull requests. It automatically adds a pull request comment showing the cost estimate difference for the planned state if a configurable percentage threshold is crossed.
+
+See the [Infracost integrations](https://www.infracost.io/docs/integrations/cicd) page for other integrations.
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,53 @@
-provider "aws" {
-  region                      = "us-east-1" # <<<<< Try changing this to eu-west-1 to compare the costs
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
 }
 
-resource "aws_instance" "web_app" {
-  ami           = "ami-674cbc1e"
-  instance_type = "m5.4xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
+resource "azurerm_linux_virtual_machine" "my_vm" {
+  name                = "basic_a2"
+  resource_group_name = "fake_resource_group"
+  location            = "eastus"
 
-  root_block_device {
-    volume_size = 50
+  size           = "Basic_A2" # <<<<< Try changing this to Basic_A4 to compare the costs
+  admin_username = "fakeuser"
+  admin_password = "fakepass"
+
+  network_interface_ids = [
+    "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testrg/providers/Microsoft.Network/networkInterfaces/fakenic",
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  ebs_block_device {
-    device_name = "my_data"
-    volume_type = "io1"                     # <<<<< Try changing this to gp2 to compare costs
-    volume_size = 1000
-    iops        = 800
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
   }
 }
 
-resource "aws_lambda_function" "hello_world" {
-  function_name = "hello_world"
-  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
-  handler       = "exports.test"
-  runtime       = "nodejs12.x"
-  memory_size   = 512                      # <<<<< Try changing this to 512 to compare costs
+resource "azurerm_app_service_plan" "elastic" {
+  name                = "api-appserviceplan-pro"
+  location            = "eastus"
+  resource_group_name = "fake_resource_group"
+  kind                = "elastic"
+  reserved            = false
+
+  sku {
+    tier     = "Basic" 
+    size     = "EP2" 
+    capacity = 1
+  }
+}
+
+resource "azurerm_function_app" "my_function" {
+  name                       = "hello-world"
+  location                   = "uksouth" # <<<<< Try changing this to EP3 to compare the costs
+  resource_group_name        = "fake_resource_group"
+  app_service_plan_id        = azurerm_app_service_plan.elastic.id
+  storage_account_name       = "fakestorageaccountname"
+  storage_account_access_key = "fake_storage_account_access_key"
 }
